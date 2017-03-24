771699f9333861474a9e90244e42f9f49040f5a4
Pettyfer <pettyferlove@live.cn>
2017-03-24 13:08:42 +0800

    commit

--- resources/js/wechat_core.js
+++ resources/js/wechat_core.js
@@ -63,6 +63,13 @@
         WeChat.hideIndicator();
     })
 
+    function addLocalData(data){
+        if(data.result.length>0){
+            for(i=0;i<data.result.length;i++){
+                window.localStorage.setItem(data.result[i].tsm5,JSON.stringify(data.result[i]))
+            }
+        }
+    }
 
     function initBookTabPage(tabName){
         var _index = 2;
@@ -73,6 +80,7 @@
         };
         api.getBookList(data, function(data) {
             var data = JSON.parse(data);
+            addLocalData(data);
             if(data.result.length < 9) {
                 WeChat.detachInfiniteScroll($$('.infinite-scroll'));
                 $$('.infinite-scroll-preloader').hide();
@@ -94,7 +102,7 @@
             api.getBookList(data, function(data) {
                 var data = JSON.parse(data);
                 var _temp = {};
-
+                addLocalData(data);
                 if(data.result == 0) {
                     // 加载完毕，则注销无限加载事件，以防不必要的加载
                     WeChat.detachInfiniteScroll($$("#"+tabName+" .infinite-scroll"));
@@ -181,19 +189,15 @@
     $$(document).on("click", ".page-content .card", function() {
         var card = $$(this);
 
-        //测试数据
-        var _temp={"book_id":123456,"book_content":"sssadadadadd","book_img":2000,"book_num":2256};
-        window.localStorage.setItem($$(this).find("input").val(),JSON.stringify(_temp))
+        var data = JSON.parse(window.localStorage.getItem(card.find("input").val()));
 
 
-
+        //resources/img/book/book_3.jpeg
         var modal = WeChat.modal({
             afterText:  '<div class="card card-header-pic wx_popup_card popup_header_pic">'
-            +'<div style="background-image:url(resources/img/book/book_3.jpeg)" valign="bottom" class="card-header color-white no-border"></div>'
+            +'<div style="background-image:url('+data.picture+')" valign="bottom" class="card-header color-white no-border"></div>'
             +'<div class="card-content">'
-            +'<div class="card-content-inner">噫吁嚱，危乎高哉！蜀道之难，难于上青天！蚕丛及鱼凫，开国何茫然！尔来四万八千岁，不与秦塞通人烟。西当太白有鸟道，可以横绝峨眉巅。地崩山摧壮士死，然后天梯石栈相钩连。上有六龙回日之高标，下有冲波逆折之回川。黄鹤之飞尚不得过，猿猱欲度愁攀援。青泥何盘盘，百步九折萦岩峦。扪参历井仰胁息，以手抚膺坐长叹。'
-            +'问君西游何时还？畏途巉岩不可攀。但见悲鸟号古木，雄飞雌从绕林间。又闻子规啼夜月，愁空山。蜀道之难，难于上青天，使人听此凋朱颜！连峰去天不盈尺，枯松倒挂倚绝壁。飞湍瀑流争喧豗，砯崖转石万壑雷。其险也如此，嗟尔远道之人胡为乎来哉！'
-+'剑阁峥嵘而崔嵬，一夫当关，万夫莫开。所守或匪亲，化为狼与豺。朝避猛虎，夕避长蛇；磨牙吮血，杀人如麻。锦城虽云乐，不如早还家。蜀道之难，难于上青天，侧身西望长咨嗟！</div>'
+            +'<div class="card-content-inner">'+data.context+'</div>'
         +'</div>'
         +'</div>',
             buttons: [
@@ -201,8 +205,7 @@
                     text: '👍',
                     bold: true,
                     onClick: function () {
-                        var data =JSON.parse(window.localStorage.getItem(card.find("input").val()));
-                        var _html = "<div>已获票数："+data.book_num+"票</div>";
+                        var _html = "<div>已获票数："+(Number(data.likeNum)+Number(1))+"票</div>";
                         card.find(".card-footer").html(_html)
                     }
                 },
@@ -210,7 +213,7 @@
                     text: '查看更多',
                     bold: true,
                     onClick: function () {
-
+                        WeChat.alert("投票活动还未结束，结束后将开放该功能。");
                     }
                 },
                 {
